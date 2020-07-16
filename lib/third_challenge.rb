079a3938df3d907811d68cf2d108d7c5eb2fc35f
Katie Le <lethikatie@gmail.com>
2020-07-16 21:52:36 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,7 +5,7 @@ def third_challenge
         :name => "Lord Montague", :age => "53"
         },
       :matriarch => {
-        :name => "Romeo", :age => "15", :status => "alive"
+        :name => "Lady Montague", :age => "54"
         },
       :hero => {
         :name => ""
